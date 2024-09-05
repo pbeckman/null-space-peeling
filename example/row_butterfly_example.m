44e37c8239161131d181ff3a47203824aeeaa8bd
Paul Beckman <paul.beckman@cims.nyu.edu>
2024-09-05 11:11:59 -0400

    move RBF example

--- example/row_butterfly_example.m
+++ example/row_butterfly_example.m
@@ -1,11 +1,13 @@
 %% Compute row butterfly factorization from matvecs
 
 % size of matrix
-n   = 45;
+n = 128;
 % rank of block row
-r   = 10;
+r = 16;
+% amount of oversampling to do (usually 5 or so is fine, see Halko et al 2011)
+p = 5;
 % number of random matvecs
-s   = r + 10;
+s   = r + p;
 % level of factorization hierarchy
 lvl = floor(log2(n)-log2(r));
 % construct index tree
@@ -14,11 +16,11 @@ tree = IndexTree(n, lvl);
 fprintf("\nn: %i, r: %i, s: %i, level: %i\n", n, r, s, lvl)
 
 % choose an example from below
-example = "random";
+example = "DFT";
 
 if example == "low-rank"
     % symmetric low rank kernel M*M'
-    r_true = 10;
+    r_true = 4;
     M = flip(sort(rand(n, r_true)));
     fwd = @(v) M*(M'*v);
     adj = fwd;
@@ -32,12 +34,30 @@ elseif example == "gaussian"
     fwd = @(v) ifft(G' .* fft(v, [], 1), [], 1);
     adj = fwd;
 elseif example == "DFT"
+    if mod(log2(n),1) ~= 0 || mod(log2(r),1) ~= 0
+        error("For example 'DFT', n and r must both be powers of 2.")
+    end
+
+    % bit-reversal ordering, which happens to be equivalent to the
+    % recursive partitioning of even and odd indices in the radix-2 FFT
+    pm = bin2dec(flip(dec2bin(0:n-1), 2)) + 1;
+    permute = @(v) v(pm,:);
+
     % apply the DFT matrix using the FFT
-    fwd = @(v) 1/sqrt(n) *  fft(v, [], 1);
-    adj = @(v)   sqrt(n) * ifft(v, [], 1);
+    fwd = @(v) 1/sqrt(n) * permute( fft(permute(v), [], 1));
+    adj = @(v)   sqrt(n) * permute(ifft(permute(v), [], 1));
+elseif example == "FIO"
+    xs = linspace(0, 1, n+1);
+    xs = xs(1:end-1);
+    fs = -floor(n/2):floor(n/2)-1;
+    c   = @(x) (2 + sin(2*pi*x)) / 8;
+    phi = @(f, x) x'*f + c(x')*abs(f);
+    K   = exp(2*pi*1i*phi(xs,fs)); 
+    fwd = @(v) 1/sqrt(n) * K * fft(v, [], 1);
+    adj = @(v)   sqrt(n) * ifft(K' * v, [], 1);
 elseif example == "random"
-    % random butterfly matrix
-    r_true = 10;
+    % random butterfly matrix just to check self consistency
+    r_true = 4;
     B = RBFMatrix();
     B.tree = tree;
     B.V = BDMatrix(cellfun(@(idx) randn(length(range(idx)),r_true), B.tree.idx{lvl+1}, 'UniformOutput', false));
@@ -58,31 +78,11 @@ else
     error("Example '%s' not found - please run a valid example.", example)
 end
 
-
-
-% % reordered DFT matrix
-% B = RBFMatrix();
-% B.tree = tree;
-% B.V = BDMatrix(arrayfun(@(~) 1, 1:n, 'UniformOutput', false));
-% B.F = BDMatrix(arrayfun(@(~) 1, 1:n, 'UniformOutput', false));
-% for l=1:lvl
-%     W_blocks = cell(2^(l-1),1);
-%     for b=1:2^(l-1)
-%         W_blocks{b} = BlockMatrix({ ...
-%             BDMatrix(arrayfun(@(j) [1  exp(-1i*j/2^(lvl-l+1))], 0:2^(lvl-l)-1, 'UniformOutput', false)); ...
-%             BDMatrix(arrayfun(@(j) [1 -exp(-1i*j/2^(lvl-l+1))], 0:2^(lvl-l)-1, 'UniformOutput', false)); ...
-%             });
-%     end
-%     B.W{l} = BDMatrix(W_blocks);
-% end
-% fwd = @(v) B*v;
-% adj = @(v) B'*v;
-
 %% Compute factorization
 
 tic;
-verbose = false;
-A = RBFMatrix(fwd, adj, tree, r, s, verbose);
+A = RBFMatrix(fwd, adj, tree, r, s);
+% A = RBFMatrix_by_inversion(RBFMatrix(), fwd, adj, tree, r, s, 1e-16);
 t = toc;
 fprintf("\nFactorization time: %.2e s\n", t);
 
@@ -103,7 +103,6 @@ fprintf("\nRelative Frobenius error: %.3e\n", norm(K - BK, 'fro') / norm(K, 'fro
 fprintf("Dense matrix:     %.2f MB\n", whos('K').bytes * 9.53674e-7)
 fprintf("Butterfly matrix: %.2f MB\n", whos('A').bytes * 9.53674e-7)
 
-
 %% Plot dense matrix, compressed matrix, and log relative error
 
 figure(1)
@@ -115,14 +114,14 @@ subplot('Position', [0.02, 0.1, 0.3, 0.8]);
 imagesc(real(K));
 axis square
 colorbar
-title('$K$','Interpreter','latex','FontSize',24)
+title('Re($K$)','Interpreter','latex','FontSize',24)
 
 subplot('Position', [0.35, 0.1, 0.3, 0.8]);
 imagesc(real(BK));
 axis square
 clim([min(real(K), [], "all"), max(real(K), [], "all")])
 colorbar
-title(strcat('$\tilde{K}$', sprintf(' (rank %i, level %i)', r, lvl)),'Interpreter','latex','FontSize',24)
+title(strcat('Re($\tilde{K}$)', sprintf(' rank %i, level %i', r, lvl)),'Interpreter','latex','FontSize',24)
 
 ax = subplot('Position', [0.68, 0.1, 0.3, 0.8]);
 imagesc(log10(abs(K - BK) ./ abs(K)));
